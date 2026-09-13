bcc37145e1abb9ed8478bfe2eda201737048248a
Ali Aref <3li3ref7777@gmail.com>
2026-09-13 21:04:55 +0200

    first commit

---
+++ src/FooterComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="footer-container">
+        <p class="footer-text"><span>نتمنى لكم التوفيق والنجاح</span><span class="text-warning">✨</span></p>
+        <div class="footer-badge">عارف أحمد ✨</div>
+      </div>
+    </div>
+  </footer>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import MainLayoutComponent from './MainLayoutComponent.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(MainLayoutComponent).mount('#app')
